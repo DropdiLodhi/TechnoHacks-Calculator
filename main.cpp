0fc400b879288c2949cee1557151ff1aa1155563
DropdiLodhi <146704802+DropdiLodhi@users.noreply.github.com>
2023-10-28 21:45:53 +0530

    Simple Calculator

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream.h>
+using namespace std;
+main()
+{
+    char x;
+    float num1,num2;
+    while(true)
+    {
+    	cout<<"enter number first="<<endl;
+    	cin>>num1;
+    	cout<<"enter number second="<<endl;
+    	cin>>num2;
+        cout<<"press a for addition="<<endl;
+        cout<<"press b for subtraction="<<endl;
+        cout<<"press c for multiplication="<<endl;
+        cout<<"press d for division="<<endl;
+        cin>>x;
+        if(x=='a')
+        {
+        	cout<<"addition="<<num1+num2<<endl;
+        }
+        else if(x='b'){
+        	cout<<"subtraction="<<num1-num2<<endl;
+        }
+        else if(x='c'){
+        	cout<<"multiplication="<<num1*num2<<endl;
+        }
+        else if(x='d'){
+        	cout<<"division="<<num1/num2<<endl;
+        }
+        else{
+        	cout<<"invalid"<<endl;
+        } 
+    }
+}
